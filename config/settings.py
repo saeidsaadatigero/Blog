8604aacd2d0dea6bce5e25a6e90f3a1ed4bf6d7e
Saeid Saadatigero <121683582+saeidsaadatigero@users.noreply.github.com>
2025-01-05 02:32:38 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -128,14 +128,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-#OR
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
+
 
 
 # Default primary key field type
@@ -157,9 +153,13 @@ MEDIA_URL = '/media/'
 # settings.py
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
 
-
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = 'static/'
+# STATICFILES_DIRS = [BASE_DIR / 'static']  # برای development
+#OR
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR / 'staticfiles'   # برای production
